de56d8145aafe76eaddfcbd25c1d450dbb08e560
DawonJeon <dawon3024@naver.com>
2021-12-06 15:05:23 +0900

    Feat: make calculate and execute

--- 김태원/calculate.js
+++ 김태원/calculate.js
@@ -3,9 +3,7 @@ let subtract=(a ,b)=>a - b;
 let multiplay=(a, b)=>a * b;
 let divide=(a, b)=>a / b;
 
-console.log(add(1,2));
-console.log(subtract(10,2));
-console.log(multiplay(9,9));
+console.log(add(100,5));
+console.log(subtract(230,86));
+console.log(multiplay(15,19));
 console.log(divide(10,2));
-
-//feat 
