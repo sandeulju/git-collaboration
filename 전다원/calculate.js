2f40ea549f47fbf901655811870861bdd4df1484
DawonJeon <dawon3024@naver.com>
2021-12-06 16:32:29 +0900

    test

--- 전다원/calculate.js
+++ 전다원/calculate.js
@@ -1,11 +1,12 @@
+
 let add=(a, b)=>a + b;
 let subtract=(a ,b)=>a - b;
 let multiplay=(a, b)=>a * b;
 let divide=(a, b)=>a / b;
 
 console.log(add(1,2));
-console.log(subtract(10,2));
+console.log(subtract(20,5));
 console.log(multiplay(9,9));
 console.log(divide(10,2));
 
-//feat 
+
